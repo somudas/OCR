5d9fa8b608ec210c56a6bb7ae23b9ebd426d93bd
Soumyadeep  Das <34862954+somudas@users.noreply.github.com>
2023-01-18 09:44:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pytesseract
+import cv2
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread('img_3.jpg')
+
+_, img = cv2.threshold(img,100,255,cv2.THRESH_BINARY)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV_YV12)
+
+print(pytesseract.image_to_string(img))
+
+cv2.imshow('hemlo', img)
+cv2.waitKey(0)
